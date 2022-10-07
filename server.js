dbd38d715bec9d390cfda6d973c60b2a91e0c715
Robin Schmidt <schmidt.rbn@gmail.com>
2022-10-07 10:26:42 +0200

    BUGFIX: Typo missing dot in port declaration

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const PORT = process.envPORT || 8080;
+const PORT = process.env.PORT || 8080;
 const db = require("./db");
 
 app.use(express.urlencoded({ extended: true }));
