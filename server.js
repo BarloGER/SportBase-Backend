e6984f2245442feeaf86c889d1c5f5459149f424
Niklas <Niklas-Riebesell@outlook.de>
2022-10-07 09:28:30 +0200

    basic server with DB

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = process.envPORT || 8080;
+const db = require("./db");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+db();
+// STARTSEITE
+app.get("/", (req, res) => {
+  res.send("Willkommen zu unserem Abschlussprojekt");
+});
+
+// LISTENING
+app.listen(PORT, () => {
+  console.log(`Server is running on Port: ${PORT}`);
+});
