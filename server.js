550685a23ccf66806ed0f95c317da8689116c88a
Niklas <Niklas-Riebesell@outlook.de>
2022-10-13 15:55:22 +0200

    added auth document

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const db = require("./db");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "*" }));
 db();
 // STARTSEITE
 app.get("/", (req, res) => {
