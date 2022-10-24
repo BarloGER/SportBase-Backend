68d1a71a52b370c5dbcc2bdee50d8982df76eb02
Niklas <Niklas-Riebesell@outlook.de>
2022-10-24 12:34:37 +0200

    changed opponent

--- models/Event.js
+++ models/Event.js
@@ -6,7 +6,7 @@ const Event = new Schema({
   startDate: { type: Date, required: true },
   endDate: { type: Date, required: true },
   createdAt: { type: Date, default: Date.now },
-  opponent: { type: String, default: "Looser" },
+  opponent: { type: String },
   activePlayers: { type: Array },
   reservePlayers: { type: Array },
   lineUp: { type: String },
